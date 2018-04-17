ce1c604316cb9be1b85dd841fdaf7cf14642a08f
Georg Braun <mail@georg-braun.de>
2018-04-17 17:41:30 +0200

    Struktur für die Grafen Algorithmen angelegt (Strategy Pattern)

---
+++ GraphLibrary/GraphLibrary/Algorithm/IGraphAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using GraphLibrary.DataStructure;
+
+namespace GraphLibrary.Algorithm
+{
+    interface IGraphAlgorithm
+    {
+        IGraph UsedGraph { get; }
+
+        void Execute();
+
+    }
+}
