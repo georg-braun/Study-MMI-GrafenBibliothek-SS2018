e71e7165c89b6aa2ded02c6042d98138273a54c7
Georg Braun <mail@georg-braun.de>
2018-04-25 18:15:14 +0200

    Prim Cleanup

--- GraphLibrary/GraphLibrary/Algorithm/PrimAlgorithm.cs
+++ GraphLibrary/GraphLibrary/Algorithm/PrimAlgorithm.cs
@@ -30,43 +30,40 @@ namespace GraphLibrary.Algorithm
             // Annahme: Der übergebene Graf ist zusammenhängend 
 
             var hMinimalSpanningTree = new Graph();
-            var hEdges = FUsedGraph.GetEdgeIndices();
             var hNodeDictionary = FUsedGraph.GetNodeIndices();
             
-            var hPossibleEdgeToNodeIdIndex = new Dictionary<Edge,int>();
-            var hReachableNodeIdWithEdgeIndex = new Dictionary<int, Edge>();
+            var hPossibleEdgeToNodeIdIndex = new Dictionary<Edge,int>(FUsedGraph.GetEdgeIndices().Count);
+            var hReachableNodeIdWithEdgeIndex = new Dictionary<int, Edge>(FUsedGraph.GetNodeIndices().Count);
             var hEdgeWeightAscComparer = new EdgeWeightComparerAsc();
-          
-        
-            var hVisitedNodes = new HashSet<int>();           
+            
+
+              var hVisitedNodes = new HashSet<int>();           
 
             // Angabe eines Startknoten. Ein Knoten der mit der ersten Kante aus der Globalen Kantenliste erreicht wird.
             // Eine Fake-Edge für den Einstieg in den Algorithmus
             var hFakeEdge = new UndirectedEdge(new Node(-1), new Node(-1),new CostWeighted(0.0));
             hPossibleEdgeToNodeIdIndex.Add(hFakeEdge, 0);
             hReachableNodeIdWithEdgeIndex.Add(0, hFakeEdge);
-            //hUsableEdgesAsc.Add(hFakeEdge);
+            var hUsableEdgesAsc = new List<Edge>(hReachableNodeIdWithEdgeIndex.Values);
+            
 
             double hWeightValue = 0.0;
+            int hTotalNodeCount = hNodeDictionary.Count;
 
-            while (hVisitedNodes.Count < hNodeDictionary.Count)
+            while (hVisitedNodes.Count < hTotalNodeCount)
             {
-                
-
-                var hUsableEdgesAsc = new List<Edge>(hReachableNodeIdWithEdgeIndex.Values);
                 hUsableEdgesAsc.Sort(hEdgeWeightAscComparer);
 
                 // Finde niedrigste Kante die zu einem unbesuchten Knoten führt
                 var hSmallestPossibleEdge = hUsableEdgesAsc.First();
                 var hReachableNodeIdWithSmallestEdge = hPossibleEdgeToNodeIdIndex[hSmallestPossibleEdge];
-
-
+                
                 // Knoten besucht
                 hVisitedNodes.Add(hReachableNodeIdWithSmallestEdge);
                 // Indizes anpassen
                 hPossibleEdgeToNodeIdIndex.Remove(hSmallestPossibleEdge);
                 hReachableNodeIdWithEdgeIndex.Remove(hReachableNodeIdWithSmallestEdge);
-                //hUsableEdgesAsc.Remove(hSmallestPossibleEdge);
+                hUsableEdgesAsc.Remove(hSmallestPossibleEdge);
                 
                 hWeightValue += hSmallestPossibleEdge.GetWeightValue();
 
@@ -74,27 +71,29 @@ namespace GraphLibrary.Algorithm
                 // Diese Infos werden nun in die Erreichbaren Liste ergänzt.
                 foreach (var hNewEdgeInfos in hNodeDictionary[hReachableNodeIdWithSmallestEdge].NeighboursEdges)
                 {
-                    if (!hVisitedNodes.Contains(hNewEdgeInfos.Node.Id))
+                    var hNeighbourNodeId = hNewEdgeInfos.Node.Id;
+                    var hEdgeToNeighbour = hNewEdgeInfos.Edge;
+                    if (!hVisitedNodes.Contains(hNeighbourNodeId))
                     {
                         // Knoten wurde noch nicht besucht
-                        if (hReachableNodeIdWithEdgeIndex.ContainsKey(hNewEdgeInfos.Node.Id))
+                        if (hReachableNodeIdWithEdgeIndex.ContainsKey(hNeighbourNodeId))
                         {
                             // Es gibt schon einen Weg zu diesem Knoten
-                            if (hNewEdgeInfos.Edge.GetWeightValue() < hReachableNodeIdWithEdgeIndex[hNewEdgeInfos.Node.Id].GetWeightValue())
+                            if (hEdgeToNeighbour.GetWeightValue() < hReachableNodeIdWithEdgeIndex[hNeighbourNodeId].GetWeightValue())
                             {
                                 // Mit der neuen Kante kommt man aber günstiger zu dem Knoten
-                                //hUsableEdgesAsc.Remove(hReachableNodeIdWithEdgeIndex[hNewEdgeInfos.Node.Id]); // bisherige Kante in der sortierten Kanten-Liste entfernen
-                                //hUsableEdgesAsc.Add(hNewEdgeInfos.Edge);
-                                hPossibleEdgeToNodeIdIndex.Add(hNewEdgeInfos.Edge, hNewEdgeInfos.Node.Id);
-                                hReachableNodeIdWithEdgeIndex[hNewEdgeInfos.Node.Id] = hNewEdgeInfos.Edge;
+                                hUsableEdgesAsc.Remove(hReachableNodeIdWithEdgeIndex[hNeighbourNodeId]); // bisherige Kante in der sortierten Kanten-Liste entfernen
+                                hUsableEdgesAsc.Add(hEdgeToNeighbour);
+                                hPossibleEdgeToNodeIdIndex.Add(hEdgeToNeighbour, hNeighbourNodeId);
+                                hReachableNodeIdWithEdgeIndex[hNeighbourNodeId] = hEdgeToNeighbour;
                             }
                         }
                         else
                         {
                             // Es gab bisher noch keinen Weg zu diesem Knoten. Also wird er nun aufgenommen
-                            //hUsableEdgesAsc.Add(hNewEdgeInfos.Edge);
-                            hPossibleEdgeToNodeIdIndex.Add(hNewEdgeInfos.Edge, hNewEdgeInfos.Node.Id);
-                            hReachableNodeIdWithEdgeIndex.Add(hNewEdgeInfos.Node.Id, hNewEdgeInfos.Edge);
+                            hUsableEdgesAsc.Add(hEdgeToNeighbour);
+                            hPossibleEdgeToNodeIdIndex.Add(hEdgeToNeighbour, hNeighbourNodeId);
+                            hReachableNodeIdWithEdgeIndex.Add(hNeighbourNodeId, hEdgeToNeighbour);
                         }
                     }
                 }
@@ -104,6 +103,9 @@ namespace GraphLibrary.Algorithm
             Console.WriteLine("Prim-Zeit:\t" + FStopwatch.ElapsedMilliseconds.ToString() + " ms");
             Console.WriteLine("Prim-Kosten:\t " + hWeightValue.ToString());
         }
+
+        
+
 
         private void AddPossibleEdgeToNodeInfo(Dictionary<int, List<Edge>> _Dict, int _NodeId, Edge _Edge)
         {
